62912d216aa2ecccc0258d8febf9d449130ef3cb
natalia <nespinozabar@gmail.com>
2024-11-19 10:22:31 -0500

    strapi-msql-cloudinary

---
+++ config/middlewares.ts
@@ -0,0 +1,34 @@
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:3000', 'https://www.ariapp.ai','https://dev.ariapp.ai' ], // Cambia esto a los dominios permitidos
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
---
+++ config/plugins.ts
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME', 'diwel1yye'),
+          api_key: env('CLOUDINARY_API_KEY', '124848985197925'),
+          api_secret: env('CLOUDINARY_API_SECRET', 'oEJr17hGPL7XqlvKCBtjTqj-JF4'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+  });
+  
